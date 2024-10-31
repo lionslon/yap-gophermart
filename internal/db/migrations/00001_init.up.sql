7d234278bb37808236d21a37c115e744e60e7841
Roman Osipov <r.osipov@MAC2327.local>
2024-10-31 21:28:41 +0300

    Reconfigured database

--- internal/db/migrations/00001_init.up.sql
+++ internal/db/migrations/00001_init.up.sql
@@ -1,42 +1,39 @@
-BEGIN TRANSACTION;
-
+begin transaction;
+create type order_status as enum ('NEW', 'PROCESSING', 'INVALID', 'PROCESSED');
 -- Пользователи
-CREATE TABLE Users(
+create table users(
                       id uuid default gen_random_uuid(),
                       login varchar(200) unique not null,
                       pass varchar(64) not null,
-
                       primary key (id)
 );
-
 -- Заказы
-CREATE TABLE Orders(
+create table orders(
                        id uuid default gen_random_uuid(),
-                       uploaded timestamp with time zone,
+                       uploaded timestamp with time zone not null,
                        number numeric unique not null,
-                       userId uuid not NULL,
-                       sum integer NOT null,
-                       status VARCHAR(16) not null,
-
+                       userid uuid not null,
+                       sum double precision not null,
+                       status order_status not null,
                        primary key (id),
-                       foreign key (userId) references Users (id)
+                       foreign key (userid) references users (id)
 );
-
--- История начисления и списания баланса пользователя
-CREATE TABLE Balances(
-                         date timestamp with time zone,
-                         userId uuid not null,
-                         sum integer NOT null,
-
-                         foreign key (userId) references Users (id)
+-- История списания баланса пользователя
+create table withdrawals(
+                            seq int generated always as identity,
+                            date timestamp with time zone not null,
+                            ordernumber numeric not null,
+                            userid uuid not null,
+                            sum double precision not null,
+                            primary key (seq),
+                            foreign key (userid) references users (id)
 );
-
--- Для хранения текущего баланса, чтобы не считать по Balances
-CREATE TABLE CurrentBalances(
-                                userId uuid not null,
-                                sum integer not null,
-
-                                foreign key (userId) references Users (id)
+-- Для хранения текущего баланса, чтобы не считать по balances
+create table currentbalances(
+                                seq int generated always as identity,
+                                userid uuid unique not null,
+                                sum double precision not null,
+                                primary key (seq),
+                                foreign key (userid) references users (id)
 );
-
-COMMIT;
+commit;
